cd8e9a253654857f4797f49b986e36b01df85360
Esha <101323811+Esha-2310@users.noreply.github.com>
2024-04-06 17:32:56 +0530

    Add files via upload

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,23 @@
+const mongoose=require("mongoose")
+/*
+const connect=mongoose.connect("")
+connect.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log("failed to connect");
+})*/
 
+const connectDB = async () => {
+  
+    try {
+      mongoose.set('strictQuery',false);
+      const conn = await mongoose.connect(process.env.MONGODB_URL);
+      console.log("Database Connected");
+    } catch (error) {
+      console.log(error);
+    }
+  
+  }
+
+module.exports=connectDB;
